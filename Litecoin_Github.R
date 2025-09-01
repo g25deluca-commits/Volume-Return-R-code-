173d38b9060726e82a20de24af79af2486490e15
Giovanni De Luca <g25deluca@gmail.com>
2025-09-01 11:48:46 +0200

    Update Litecoin_Github.R

--- Litecoin_Github.R
+++ Litecoin_Github.R
@@ -1,4 +1,10 @@
 # This file was created by Giovanni De Luca and Andrea Montanino  
+# List of packages
+library(quantmod)
+library(forecast)
+library(rugarch)
+library(copula)
+library(VineCopula)
 # Downloading the Litecoin data from Yahoo Finance for a specific period
 litecoin = getSymbols("LTC-USD", from= "2018-01-01", to="2025-04-30", src="yahoo", auto.assign = FALSE)
 
@@ -141,4 +147,5 @@ lambda_uu = fit_Mix_cop_pobs@estimate[3]*BiCopPar2TailDep(6,fit_Mix_cop_pobs@est
 round(lambda_uu,3)
 lambda_lu = fit_Mix_cop_pobs@estimate[4]*BiCopPar2TailDep(6,fit_Mix_cop_pobs@estimate[2])$upper
 round(lambda_lu,3)
+
 
